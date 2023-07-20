65199679514b327637d039243a5316e1a7b7a3c0
Mykal <mykal.stele@gmail.com>
2023-07-20 19:14:42 +0630

    OMg i got this first try!

---
+++ Functions.py
@@ -0,0 +1,9 @@
+def list_benifits():
+    return ["More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together"]
+
+def build_sentence(info):
+    for list in x:
+            print(list, " is a benefit of functions!")
+
+x = list_benifits()
+build_sentence(x)
