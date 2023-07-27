334fb9311667a4a9d6076ce1509057ecbcfa3aaa
Mykal <mykal.stele@gmail.com>
2023-07-27 10:53:52 +0630

    commit

--- LearnPython/Classes and Objects.py
+++ LearnPython/Classes and Objects.py
@@ -21,3 +21,17 @@ car2 = Vehicle("Jump", "van", "blue", 10000.00)
 # test code
 print(car1.description())
 print(car2.description())
+
+
+
+#same code but shorter, the code dont use __init__
+# #class Vehicle:
+#   def final(self, name, type, color, value):
+#
+#        val = "car name is: %s, type is %s, color is %s, value is $%d" % (name, type, color, value)
+#        return val
+
+#car1 = Vehicle()
+#car2 = Vehicle()
+#print(car1.final("Fer", "convertible", "red", 60000.00))
+#print(car2.final("Jump", "van", "blue", 10000.00))
