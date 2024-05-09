e2bf5afff12ccfbb03e790e4ab4b17126c46a4f8
Mykal Steele <130909466+Mykal-Steele@users.noreply.github.com>
2024-05-09 16:28:56 +0700

    Update kth.py

--- PythonScripts/kth.py
+++ PythonScripts/kth.py
@@ -1,33 +1,35 @@
-def kthDigit(x, b, k):
-    if k == 0: 
-        return x % b
-    else:
-        for i in range(k):  #The will divide the x(always base 10) by the base we want it to change, becasue we need the remainder of each value after devision (this is how we turn base 10 to base of anything)
-            x //= b
-        return x % b
 
-# Example usage:
-print(kthDigit(x = int(input("x = ")),b = int(input("base = ")),k = int(input("k = "))))
+# This is a better, doesn't follow the special rule
+# def kthDigit(x, b, k):
+#     if k == 0: 
+#         return x % b
+#     else:
+#         for i in range(k):  #The will divide the x(always base 10) by the base we want it to change, becasue we need the remainder of each value after devision (this is how we turn base 10 to base of anything)
+#             x //= b
+#         return x % b
+
+# # Example usage:
+# print(kthDigit(x = int(input("x = ")),b = int(input("base = ")),k = int(input("k = "))))
 
 
-# This is my initial code lmao. I forgot that 'for' loop existed
+# i only used if statements here, but the code sucks
 
 
-# def kthDigit(x,b,k):
-#     if k == 0:
-#         output = x % b
-#         return int(output)
-#     elif k == 1:
-#         x = x/b
-#         output = x % b
+def kthDigit(x,b,k):
+    if k == 0:
+        output = x % b
+        return int(output)
+    elif k == 1:
+        x = x/b
+        output = x % b
 
-#         return int(output)
-#     elif k == 2:
-#         x = x/b
-#         x = x/b
-#         output = x% b
-#         return int(output)
+        return int(output)
+    elif k == 2:
+        x = x/b
+        x = x/b
+        output = x% b
+        return int(output)
     
 
-# result = kthDigit(x = int(input("x = ")),b = int(input("base = ")),k = int(input("k = ")))
-# print(result)
+result = kthDigit(x = int(input("x = ")),b = int(input("base = ")),k = int(input("k = ")))
+print(result)
