6be192c0b6cfdff7b08ad8c69d409b493e6e697d
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2024-10-31 09:49:11 +0700

    fixed spacing

--- PythonScripts/Homework/a4/multiplek.py
+++ PythonScripts/Homework/a4/multiplek.py
@@ -10,7 +10,7 @@ while True:  # Start an infinite loop
     if w == True:  # Check if it's the first iteration
         while True:  # Start an inner infinite 
             print("This will check if ALL the number you typed are the multiples of k")
-            k = input("Type in the integer k: $")  # Prompt user to input k
+            k = input("Type in the integer k: $ ")  # Prompt user to input k
             try:
                 k = int(k)  # Try to convert k to an integer
                 break  # If successful, exit the inner loop
@@ -19,7 +19,7 @@ while True:  # Start an infinite loop
         i = input("You could type multiple numbers. \nType your first number for list 'lst' (or 'Done' to stop): $")  # Prompt user for the first list element
         w = False  # Set flag to False after first iteration
     else:
-        i = input("Type your Next numbers (or 'Done' to stop): $")  # Prompt user for additional list elements
+        i = input("Type your Next numbers (or 'Done' to stop): $ ")  # Prompt user for additional list elements
 
     if i.lower() == "done" or str(k).lower() == "done":  # Check if user wants to stop
         break  # Exit the outer loop
